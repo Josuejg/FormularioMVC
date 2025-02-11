169eb3e3b6700124a1068d11f5486c7a66df9f6c
JOSUEJIMENEZ <cocho271087@gmail.com>
2025-02-10 19:23:15 -0600

    Agregar archivos de proyecto.

---
+++ Models/ContactMessage.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormularioMVC.Models
+{
+
+    public class ContactMessage
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
+        [StringLength(150)]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "El mensaje no puede estar vacío.")]
+        [StringLength(255)]
+        public string Mensaje { get; set; }
+    }
+
+}
